7ac2c19e933ded8d0370401f774e634f24c792fb
Jack Hougland <jack.hougland@gmail.com>
2020-05-17 14:43:37 -0500

    Initial commit

---
+++ CalculatorDemo/CalculatorDemo/Calculator.cs
@@ -0,0 +1,72 @@
+﻿using SYS = System;
+
+namespace CalculatorDemo
+{
+  /// <summary>
+  /// This class provides a subset of capabilities
+  /// expected for a calculator to perform.
+  /// </summary>
+  [SYS.Serializable]
+  public static class Calculator
+  {
+    #region Main method
+
+    /// <summary>
+    /// Starter method for the application.
+    /// </summary>
+    /// <param name="args">The parameter is not used.</param>
+    static void Main( string[] args )
+    {     
+      // No implementation necessary.  The purpose of this application
+      // is to just get some basic experience with the concept
+      // of data driven unit testing on a calculator class.  The
+      // calculator class will be exercised from a unit testing project.
+    }
+
+    #endregion
+    
+    #region Public static methods
+
+    /// <summary>
+    /// This method computes and returns the sum of the two provided numbers.
+    /// </summary>
+    /// <param name="number1">The first number in addition operation.</param>
+    /// <param name="number2">The second number in the addition operation.</param>
+    /// <returns>See method summary.</returns>
+    public static double Add( double number1, double number2 ) => number1 + number2;
+
+    /// <summary>
+    /// This method subtracts the second number from the first number and returns the difference.
+    /// </summary>
+    /// <param name="number1">The first number in the subtraction operation.</param>
+    /// <param name="number2">The second number in the subtraction operation.</param>
+    /// <returns>See method summary.</returns>
+    public static double Subtract( double number1, double number2 ) => number1 - number2;
+
+    /// <summary>
+    /// This method multiples the first and second number provided and returns the product.
+    /// </summary>
+    /// <param name="number1">The first number in the multiplication operation.</param>
+    /// <param name="number2">The second number in the multiplication operation.</param>
+    /// <returns>See method summary.</returns>
+    public static double Multiply( double number1, double number2 ) => number1 * number2;
+
+    /// <summary>
+    /// This method divides the second number from the first and returns the result.
+    /// </summary>
+    /// <param name="number1">The first number in the division operation.</param>
+    /// <param name="number2">The second number in the division operation.</param>
+    /// <returns>See method summary.</returns>
+    /// <exception cref="SYS.ArgumentException">Thrown if the second number is 0.</exception>
+    public static double Divide( double number1, double number2 )
+    {
+      if ( number2.Equals( 0.0 ) )
+      {
+        throw new SYS.ArgumentException( "The second number in the division cannot be zero." );
+      }
+      return number1 / number2;
+    }
+
+    #endregion
+  }
+}
---
+++ CalculatorDemo/CalculatorDemoTests/CalculatorTests.cs
@@ -0,0 +1,114 @@
+﻿using MVST = Microsoft.VisualStudio.TestTools.UnitTesting;
+using CD = CalculatorDemo;
+using SYS = System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorDemo.Tests
+{
+  /// <summary>
+  /// This class contains the tests for the
+  /// <see cref="CD.Calculator"/> class.
+  /// </summary>
+  [MVST.TestClass()]
+  [SYS.Serializable]
+  public class CalculatorTests
+  {
+    #region Private member objects
+
+    private MVST.TestContext m_testContext;
+
+    #endregion
+
+    #region Public properties
+
+    /// <summary>
+    /// Gets and sets the test context that provides information to the unit tests.
+    /// </summary>
+    public MVST.TestContext TestContext
+    {
+      get
+      {
+        return m_testContext;
+      }
+      set
+      {
+        m_testContext = value;
+      }
+    }
+
+    #endregion 
+
+    #region Public test methods
+
+    /// <summary>
+    /// This data driven test accepts various sets of input data to ensure
+    /// we get the expected sum.
+    /// </summary>
+    /// <param name="number1">The first number in an addition operation.</param>
+    /// <param name="number2">The second number in an addition operation.</param>
+    /// <param name="expectedSum">The expected sum.</param>
+    [MVST.DataTestMethod]
+    [DataRow( 10, 20, 30 )]
+    [DataRow( -5, -15, -20 )]
+    [DataRow( -8, 15, 7 )]
+    public void AdditionTest( double number1, double number2, double expectedSum ) =>    
+      MVST.Assert.AreEqual( CD.Calculator.Add( number1, number2 ), expectedSum );
+
+    /// <summary>
+    /// This data driven test accepts various sets of input data to ensure
+    /// we get the expected difference.
+    /// </summary>
+    /// <param name="number1">The first number in the subtraction operation.</param>
+    /// <param name="number2">The second number in the subtraction operation.</param>
+    /// <param name="expectedDifference">The expected difference.</param>
+    [MVST.DataTestMethod]
+    [DataRow( 30, 20, 10 )]
+    [DataRow( -5, -15, 10 )]
+    [DataRow( -8, 15, -23 )]
+    public void SubtractionTest( double number1, double number2, double expectedDifference ) =>
+      MVST.Assert.AreEqual( CD.Calculator.Subtract( number1, number2 ), expectedDifference );
+
+    /// <summary>
+    /// This data driven test accepts various sets of input data to ensure
+    /// we get the expected product.
+    /// </summary>
+    /// <param name="number1">The first number in the multiplication operation.</param>
+    /// <param name="number2">The second number in the multiplication operation.</param>
+    /// <param name="expectedProduct">The expected product.</param>
+    [MVST.DataTestMethod]
+    [DataRow( 10, 20, 200 )]
+    [DataRow( -5, -15, 75 )]
+    [DataRow( -8, 15, -120 )]
+    public void MultiplicationTest( double number1, double number2, double expectedProduct ) =>
+      MVST.Assert.AreEqual( CD.Calculator.Multiply( number1, number2 ), expectedProduct );
+
+    /// <summary>
+    /// This data driven test accepts various sets of input data to ensure
+    /// we get the expected quotient.
+    /// </summary>
+    /// <param name="number1">The first number in the division operation.</param>
+    /// <param name="number2">The second number in the division operation.</param>
+    /// <param name="expectedQuotient">The expected quotient.</param>
+    [MVST.DataTestMethod]
+    [DataRow( 10.0, 20.0, 0.5 )]
+    [DataRow( 20, 10, 2 )]
+    [DataRow( -5.0, -15.0, (1.0/3.0) )]
+    [DataRow( -40, -10, 4 )]
+    [DataRow( 0, 1, 0 )]
+    public void DivisionTestHappyPath( double number1, double number2, double expectedQuotient ) =>
+      MVST.Assert.AreEqual( CD.Calculator.Divide( number1, number2 ), expectedQuotient );
+
+    /// <summary>
+    /// This test verifies that if we attempt to divide by zero that an exception
+    /// will be thrown by the Divide method in the <see cref="CD.Calculator"/> class.
+    /// </summary>
+    /// <param name="number1">Tee first number in the division operation.</param>    
+    [MVST.TestMethod()]
+    [DataRow( 1 )]
+    [DataRow( -1 )]
+    public void DivideByZeroCase( double number1 ) =>
+      MVST.Assert.ThrowsException<SYS.ArgumentException>( () => CD.Calculator.Divide( number1, 0 ) );    
+
+    #endregion
+  }
+}
